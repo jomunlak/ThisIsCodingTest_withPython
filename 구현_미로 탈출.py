4f525388330d2f154b1abba469d74e6aaed5cfcf
barely bear <75612776+jomunlak@users.noreply.github.com>
2021-08-14 11:23:49 +0000

    dfs

--- 구현_미로 탈출.py
+++ 구현_미로 탈출.py
@@ -3,18 +3,14 @@ from collections import deque
 n, m = map(int,input().split())
 
 newMap = []
-
 for i in range(n):
   newMap.append(list(map(int, input())))
-visited = [[0]* m for _ in range(n)]
-visited[0][0] = 1
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 q = deque()
 q.append((0,0)) # 처음 위치 좌표를 나타내는 튜플
-count = 1
 
 while q: # 큐가 빌때까지
   
@@ -32,10 +28,6 @@ while q: # 큐가 빌때까지
           q.append((x, y))
           newMap[x][y] = newMap[loc[0]][loc[1]] + 1
   
-
-  
-
 print(newMap[n-1][m-1])
-for i in newMap:
-  print(i)
+
 
