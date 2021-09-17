2ddd9ef31671d3bf213524c7fd62e21bfed46ea5
barely bear <75612776+jomunlak@users.noreply.github.com>
2021-09-17 09:49:25 +0000

    DP

--- DP_못생긴 수.py
+++ DP_못생긴 수.py
@@ -1,13 +1,23 @@
-import heapq
 
 n = int(input())
-d = [0, 1] 
-idx = 2
-for i in range(1, 50):
-  for j in range(1, 50):
-    for k in range(1, 50):
-      heapq.heappush(d, (i**2) * (j**3) * (k**5))
-print(d)
+d = [0] * (n+1)
+d[1] = 1 # 첫번째 못생긴 수는 1
 
+i2, i3, i5 = 1, 1, 1
+# 각각 못생긴수를 저장한 후 다음으로 제일 작은 못생긴수를 찾기위한 인덱스.
+#  2, 3, 5 모두 따로 저장하기때문에 항상 제일 작은 수를 구할 수 있다.
+next2, next3, next5 = 2, 3, 5
 
-
+for i in range(2, n+1):
+  d[i] = min(next2, next3, next5)
+  if d[i] == next2:
+    i2 += 1
+    next2 = d[i2] * 2
+  if d[i] == next3:
+    i3 += 1
+    next3 = d[i3] * 3
+  if d[i] == next5:
+    i5 += 1
+    next5 = d[i5] * 5
+    
+print(d[n])
