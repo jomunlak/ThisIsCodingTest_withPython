f9c9edf59e56bfadcc2bd69f80f98316d92af09b
barely bear <75612776+jomunlak@users.noreply.github.com>
2021-09-29 11:57:20 +0000

    shark

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# 아기 상어
+from collections import deque
+n = int(input())
+graph = []
+shark = (0, 0, 0)
+for i in range(n):
+  data = list(map(int, input().split()))
+  graph.append(data)
+  for j in range(n):
+    if data[j] == 9: shark = (i, j, 2)
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1] # 상 하 좌 우
 
-
+def bfs(graph, shark):
+  q = deque()
+# king joong e
